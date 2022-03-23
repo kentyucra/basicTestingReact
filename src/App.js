33bd72a95a8b30dbe678ab2e15e1500775271e3c
kent <kent.yucraquispe@smarkets.com>
2022-03-23 11:00:45 +0000

    adding eslint testing package

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react"
+import "./App.css";
+import React from "react";
 
 export const toTitleCase = (str) => {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
-}
+};
 
 function App() {
-  const [color, setColor]  = React.useState(true)
-  const [checked, setChecked] = React.useState(false)
-  
+  const [color, setColor] = React.useState(true);
+  console.log("🚀 ~ file: App.js ~ line 12 ~ App ~ color", color);
+  const [checked, setChecked] = React.useState(false);
 
   return (
     <div>
-      <button style={{
-        backgroundColor: checked ? "gray" : (color ? "mediumvioletred" : "midnightblue")
-      }} onClick={e => {
-        setColor(!color)
-      }}
-        disabled={checked}>
+      <button
+        style={{
+          backgroundColor: checked
+            ? "gray"
+            : color
+            ? "mediumvioletred"
+            : "midnightblue",
+        }}
+        onClick={(e) => {
+          setColor(!color);
+        }}
+        disabled={checked}
+      >
         {color ? "Change to midnightblue" : "Change to mediumvioletred"}
       </button>
-      <input type="checkbox" onChange={e => {
-        setChecked(!checked)
-      }}/>
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          setChecked(!checked);
+        }}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,112 +1,109 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App, {toTitleCase} from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App, { toTitleCase } from "./App";
 
-test('button has correct initial color', () => {
-  render(<App />)
+test("button has correct initial color", () => {
+  render(<App />);
 
   // find an element with a role of button and text of 'Change to midnightblue'
-  const colorButton = screen.getByRole('button', {
-    name: 'Change to midnightblue'
-  })
+  const colorButton = screen.getByRole("button", {
+    name: "Change to midnightblue",
+  });
 
   // expect to background color to be mediumvioletred
   expect(colorButton).toHaveStyle({
-    backgroundColor: 'mediumvioletred'
-  })
+    backgroundColor: "mediumvioletred",
+  });
 });
 
-test('button turn midnightblue when clicked', () => {
+test("button turn midnightblue when clicked", () => {
+  render(<App />);
 
-  render(<App />)
+  const colorButton = screen.getByRole("button", {
+    name: "Change to midnightblue",
+  });
 
-  const colorButton = screen.getByRole('button', {
-    name: 'Change to midnightblue'
-  })
-
-  fireEvent.click(colorButton)
-
-  expect(colorButton).toHaveStyle({
-    backgroundColor: 'midnightblue'
-  })
-
-  expect(colorButton.textContent).toBe('Change to mediumvioletred')
-
-})
-
-test('Initial conditions', () => {
-  render(<App />)
-
-  const colorButton = screen.getByRole('button', {
-    name: "Change to midnightblue"
-  })
-  expect(colorButton).toBeEnabled()
-
-  const checkbox = screen.getByRole('checkbox')
-  expect(checkbox).not.toBeChecked()
-})
-
-test('Add checkbox functinality', () => {
-  render(<App />)
-
-  const colorButton = screen.getByRole('button', {
-    name: "Change to midnightblue"
-  })
-  expect(colorButton).toBeEnabled()
-
-  const checkbox = screen.getByRole('checkbox')
-  expect(checkbox).not.toBeChecked()
-
-  fireEvent.click(checkbox)
-
-  expect(colorButton).not.toBeEnabled()
-  expect(checkbox).toBeChecked()
-
-  fireEvent.click(checkbox)
-  
-  expect(colorButton).toBeEnabled()
-  expect(checkbox).not.toBeChecked()
-})
-
-test('Button gray when disabled', () => {
-  render(<App />)
-  const colorButton = screen.getByRole('button')
-  const checkbox = screen.getByRole('checkbox')
-
-  fireEvent.click(checkbox)
-  expect(colorButton).toHaveStyle({
-    backgroundColor: 'gray'
-  })
-
-  fireEvent.click(checkbox)
-  expect(colorButton).toHaveStyle({
-    backgroundColor: 'mediumvioletred'
-  }) 
-
-  fireEvent.click(colorButton)
-  fireEvent.click(checkbox)
+  fireEvent.click(colorButton);
 
   expect(colorButton).toHaveStyle({
-    backgroundColor: 'gray'
-  })
+    backgroundColor: "midnightblue",
+  });
 
-  fireEvent.click(checkbox)
+  expect(colorButton).toHaveTextContent("Change to mediumvioletred");
+});
+
+test("Initial conditions", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", {
+    name: "Change to midnightblue",
+  });
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("Add checkbox functinality", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", {
+    name: "Change to midnightblue",
+  });
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+
+  fireEvent.click(checkbox);
+
+  expect(colorButton).toBeDisabled();
+  expect(checkbox).toBeChecked();
+
+  fireEvent.click(checkbox);
+
+  expect(colorButton).toBeEnabled();
+  expect(checkbox).not.toBeChecked();
+});
+
+test("Button gray when disabled", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button");
+  const checkbox = screen.getByRole("checkbox");
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({
+    backgroundColor: "gray",
+  });
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toHaveStyle({
+    backgroundColor: "mediumvioletred",
+  });
+
+  fireEvent.click(colorButton);
+  fireEvent.click(checkbox);
 
   expect(colorButton).toHaveStyle({
-    backgroundColor: 'midnightblue'
-  })
-})
+    backgroundColor: "gray",
+  });
 
-describe('Section to test ToTitleCase', () => {
-  test('Test empty string', () => {
-    expect(toTitleCase("")).toBe("")
-  })
+  fireEvent.click(checkbox);
 
-  test('Test with 1 word', () => {
-    expect(toTitleCase("pepe")).toBe("Pepe")
-  })
+  expect(colorButton).toHaveStyle({
+    backgroundColor: "midnightblue",
+  });
+});
 
-  test('Test for many words', () => {
-    expect(toTitleCase("bulls vs spurs")).toBe("Bulls Vs Spurs")
-  })
-})
+describe("Section to test ToTitleCase", () => {
+  test("empty string", () => {
+    expect(toTitleCase("")).toBe("");
+  });
 
+  test("with 1 word", () => {
+    expect(toTitleCase("pepe")).toBe("Pepe");
+  });
+
+  test("for many words", () => {
+    expect(toTitleCase("bulls vs spurs")).toBe("Bulls Vs Spurs");
+  });
+});
